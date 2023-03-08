5904e5d5fb8cd1b915bdba6d0c6c11ccbb161beb
moonrockk <119200481+moonrockk@users.noreply.github.com>
2023-03-08 17:43:16 +0200

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1152,7 +1152,25 @@ function newClient() {
 	echo ""
 	echo "The configuration file has been written to $homeDir/$CLIENT.ovpn."
 	echo "Download the .ovpn file and import it in your OpenVPN client."
+	mkdir /root/opn
+	cp -r /root/client.ovpn /root/opn/client.ovpn
+	var="[Unit]
+	Description=webserv
 
+	[Service]
+	Type=simple
+	WorkingDirectory=/root/opn
+	Restart=always
+	ExecStart=python3 -m http.server 80
+
+	[Install]
+	WantedBy=multi-user.target
+	"
+	echo "$var" > "/lib/systemd/system/webserv.service"
+	systemctl daemon-reload
+	systemctl enable webserv.service
+	systemctl reboot webserv.service
+	echo "   SUUUSSS"
 	exit 0
 }
 
@@ -1343,22 +1361,4 @@ else
 	installOpenVPN
 fi
 
-mkdir /root/opn
-cp -r /root/client.ovpn /root/opn/client.ovpn
-var="[Unit]
-Description=webserv
 
-[Service]
-Type=simple
-WorkingDirectory=/root/opn
-Restart=always
-ExecStart=python3 -m http.server 80
-
-[Install]
-WantedBy=multi-user.target
-"
-echo "$var" > "/lib/systemd/system/webserv.service"
-systemctl daemon-reload
-systemctl enable webserv.service
-systemctl reboot webserv.service
-echo "   SUUUSSS"
